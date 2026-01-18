017ad8b03d0566bd13bbc779ea8058bc191d9507
SemRodenburg <sem.rodenburg@icloud.com>
2026-01-18 19:54:09 +0100

    Small text changes

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -41,13 +41,13 @@ const Showcase = () => {
                            <span className="text-white">
                             M4, the next generation of Apple silicon
                            </span>
-                           .M4 powers
+                           . M4 powers
                         </p>
                         <p>
-                           It drives Apple Intelligence on Ipad Pro, so you can write, create, and accomplish more with ease. All in a design that's unbelievably thin, light, and powerful. 
+                           It drives Apple Intelligence on iPad Pro, so you can write, create, and accomplish more with ease. All in a design that's unbelievably thin, light, and powerful. 
                         </p>
                         <p>
-                           A brand-new display engine delivers breathtaking precision, color accuracy, and brightness. And a next-gen GPU with hardware-accelarated ray tracing brings console-level graphics to your fingertips. 
+                           A brand-new display engine delivers breathtaking precision, color accuracy, and brightness. And a next-gen GPU with hardware-accelerated ray tracing brings console-level graphics to your fingertips. 
                         </p>
                         <p className="text-primary">Learn more about Apple Intelligence</p>
                     </div>
